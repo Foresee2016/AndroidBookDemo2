6ce18a2f5ab9b341824416317d0db73d5f040d21
Foresee <1123520670@qq.com>
2018-08-12 15:27:37 +0800

    Add Question[] model and icon and TextView-Next on click.

--- app/src/main/java/org/foresee/bookdemo2/MainActivity.java
+++ app/src/main/java/org/foresee/bookdemo2/MainActivity.java
@@ -5,32 +5,68 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button mTrueBtn;
-    Button mFalseBtn;
+    private Button mTrueBtn;
+    private Button mFalseBtn;
+    private Button mNextBtn;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+    private int mCurrentIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTrueBtn=findViewById(R.id.true_btn);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+        mTrueBtn = findViewById(R.id.true_btn);
         mTrueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast msg=Toast.makeText(MainActivity.this, R.string.correct_msg, Toast.LENGTH_LONG);
-                msg.setGravity(Gravity.TOP, 0, 0);
-                msg.show();
+                checkAnswer(true);
             }
         });
-        mFalseBtn=findViewById(R.id.false_btn);
+        mFalseBtn = findViewById(R.id.false_btn);
         mFalseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast msg = Toast.makeText(MainActivity.this, R.string.incorrect_msg, Toast.LENGTH_LONG);
-                msg.setGravity(Gravity.TOP, 0,0);
-                msg.show();
+                checkAnswer(false);
             }
         });
+        mNextBtn = findViewById(R.id.next_btn);
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId= userPressedTrue == answerIsTrue ? R.string.correct_toast : R.string.incorrect_toast;
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
